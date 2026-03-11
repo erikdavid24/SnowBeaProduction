b3209af8386fd077f8eb80e88fe07ca3127bc99d
erik.osuna <erik.osuna@GYMDLBAE058314>
2026-03-11 16:19:13 -0700

    Se configuro el web config

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BAEClassLibrary;
 
 namespace SnowTrolleyProduction.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BAEController
     {
         public ActionResult Index()
         {
