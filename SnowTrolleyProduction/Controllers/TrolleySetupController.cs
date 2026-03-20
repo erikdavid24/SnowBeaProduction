4798e6b4990d8a6ef24f5c53579285840f36e644
erik.osuna <erik.osuna@GYMDLBAE058314>
2026-03-20 12:08:54 -0700

    vista de gestionde progrma ademas de ordenar el esquema de base de datos

---
+++ SnowTrolleyProduction/Controllers/TrolleySetupController.cs
@@ -0,0 +1,60 @@
+﻿using BAEClassLibrary;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using SHE.SafetyTours.Models;
+using SHE.SafetyTours.Models.Service;
+using SnowTrolleyProduction.Models;
+using System.Web.Mvc;
+
+namespace SHE.SafetyTours.Controllers
+{
+    public class TrolleySetupController : BAEController
+    {
+        TrolleySetupServices svc = new TrolleySetupServices(new BAESystemsGuaymasEntities1());
+
+        public ActionResult Index() => View();
+
+        public JsonResult Read([DataSourceRequest] DataSourceRequest req, string start, string end)
+        {
+            var data = svc.Read(start, end);
+            return Json(data.ToDataSourceResult(req), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Create([DataSourceRequest] DataSourceRequest req, TrolleySetupViewModel item)
+        {
+            if (item != null && ModelState.IsValid)
+                svc.Create(item);
+
+            return Json(new[] { item }.ToDataSourceResult(req, ModelState));
+        }
+
+        public ActionResult Update([DataSourceRequest] DataSourceRequest req, TrolleySetupViewModel item)
+        {
+            svc.Update(item);
+            return Json(new[] { item }.ToDataSourceResult(req, ModelState));
+        }
+
+        public ActionResult Delete([DataSourceRequest] DataSourceRequest req, TrolleySetupViewModel item)
+        {
+            svc.Delete(item);
+            return Json(new[] { item }.ToDataSourceResult(req, ModelState));
+        }
+
+        public JsonResult GuardarRapido(TrolleySetupViewModel item)
+        {
+            var res = new AnswerResult();
+
+            if (item == null) return Json(res);
+
+            if (string.IsNullOrEmpty(item.WorkOrder))
+            {
+                res.AddWarning("La Orden de Trabajo no puede estar vacía.");
+                return Json(res);
+            }
+
+            svc.Create(item);
+            res.AddSuccess("Trolley configurado correctamente.");
+            return Json(res);
+        }
+    }
+}
